16c5644a7a46f1f780ab15f212d35f75f2abd09a
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2014-02-21 08:48:18 +0900

    smaller fudanum for hyakunin-isshu list

--- javascripts/hyakunin-isshu-list.js
+++ javascripts/hyakunin-isshu-list.js
@@ -2,9 +2,13 @@
   $(function() {
     if ($(window).width() < $(".author").offset().left + $(".author").width()) {
       return $(".author").each(function(i, x) {
-        var xx;
+        var fn, xx, yomiku;
         xx = $(x);
-        xx.parent().find(".simonoku").parent().append("<div style='text-align: center;color: #330000;'>(" + xx.html() + ")</div>");
+        yomiku = xx.parent().find(".simonoku").parent();
+        yomiku.append("<div style='text-align: center;color: #330000;'>(" + xx.html() + ")</div>");
+        fn = xx.parent().find(".fudanum");
+        yomiku.prepend("<div style=';color: maroon;font-size: 0.8em'>[" + fn.html() + "]</div>");
+        fn.remove();
         return xx.remove();
       });
     }
